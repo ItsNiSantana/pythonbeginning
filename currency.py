2cc368e9cefdd87230dbcbde7c0400fc434ebb3e
Nicoli <103062064+ItsNiSantana@users.noreply.github.com>
2025-01-22 11:41:44 -0300

    Create currency.py

---
+++ currency.py
@@ -0,0 +1,7 @@
+pesos = int(input('What do you have left in pesos? '))
+soles = int(input('What do you have left in soles? '))
+reais = int(input('What do you have left in reais? '))
+
+USD = ((pesos*0.0002323)+(soles*0.2679744)+(reais*0.1654533))
+
+print(USD)
